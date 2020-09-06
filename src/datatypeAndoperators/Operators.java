65ea5d0fb62abb540a37e76394f9449e73f6575f
vinod kumar <vk1999devil@gmail.com>
2020-09-06 08:40:51 +0530

    adding operators

---
+++ src/datatypeAndoperators/Operators.java
@@ -0,0 +1,16 @@
+package datatypeAndoperators;
+
+public class Operators {
+    public static void main(String[] args) {
+        /*
+        Arithmetic operators
+    Assignment operators
+   Comparison operators
+   Logical operators
+  Bitwise operators
+        */
+        int a=10;
+        int b=5;
+        System.out.println(~b);
+    }
+}
