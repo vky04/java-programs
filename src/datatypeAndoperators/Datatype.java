50283f4a2ad426a2f89577b6b98efcee2f704c20
vinod kumar <vk1999devil@gmail.com>
2020-09-06 08:06:23 +0530

    Initial commit

---
+++ src/datatypeAndoperators/Datatype.java
@@ -0,0 +1,24 @@
+package datatypeAndoperators;
+public class Datatype {
+    public static void main(String[] args) {
+        //knowing about data types
+        //1.primitive
+        //2.non primitive
+        /*
+        there are 8 primitive data  types
+        1.byte-1
+       2. short-2
+        3.int-4
+        4.long-8
+       5. float-4
+        6.double-8
+        7.boolean-1
+       8. char-2
+        */
+        float value=0.1f;
+        System.out.println(value);
+        double value1=0.2d;
+        System.out.println(value1);
+    }
+
+}
